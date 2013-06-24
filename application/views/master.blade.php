8b221a9576cd9da3d118db379aadcb94f88c9dbc
John Liu <et1103@hotmail.com>
2013-06-23 17:38:06 -0700

    john install

---
+++ application/views/master.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Assignment 2</title>
+	<meta name="viewport" content="width=device-width">
+
+</head>
+<body>
+
+</body>
+</html>
